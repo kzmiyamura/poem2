3cdb6990ea59168c91d0ce2c57818b035765cb35
miyamura <kzmiyamura@gmail.com>
2017-01-09 17:37:21 +0900

    DIVE14_API基礎編1_2 コミット２回目 修正点 1.poems_controller.rbのjsonをrender json: @poemsに変更

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -5,7 +5,7 @@ class PoemsController < ApplicationController
   # GET /poems.json
   def index
     @poems = Poem.all
-    format.json { render json: @poems }
+    render json: @poems
   end
 
   # GET /poems/1
